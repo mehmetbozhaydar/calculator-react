298523525df4173f70c0fb38eb7daba25b498cb9
mehmetbozhaydar <152904834+mehmetbozhaydar@users.noreply.github.com>
2024-03-21 08:54:05 +0300

    css-update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from "react";
+import "./styles.css"
 function App() {
   return (
-    <div >
-     <h2>Hello from react!</h2>
+    <div className="calculator-grid" >
+     <div className="output">
+      <div className="previous-operend">123.122*112</div>
+      <div className="current-operend">123.122</div>
+     </div>
+     <button className="span-two">AC</button>
+     <button>DEL</button>
+     <button>/</button>
+     <button>7</button>
+     <button>8</button>
+     <button>9</button>
+     <button>*</button>
+     <button>4</button>
+     <button>5</button>
+     <button>6</button>
+     <button>+</button>
+     <button>1</button>
+     <button>2</button>
+     <button>3</button>
+     <button>-</button>
+     <button>.</button>
+     <button>0</button>
+     <button className="span-two">=</button>
     </div>
   );
 }
